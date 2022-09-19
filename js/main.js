e924a2d7c1c059779461032a6dce17a563309e62
Khudorozhkova <E.D.Khudorozhkova@urfu.me>
2022-09-19 18:28:28 +0500

    Стрелочные функции

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+const getRandomInt = (min, max) => {
+  if (min < 0 || max > 0) {
+    return -1
+  }
+  if (min > max) {
+    [min, max] = [max, min]
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
-function checkStringLenght(str, maxLenght) {
-  if (str.length <= maxLenght){
-    return true
-  }return false
-}
+const checkStringLenght = (str, max) => str.length <= max;
 
 console.log(getRandomInt(0, 141))
 console.log(checkStringLenght('Привет', 140))
