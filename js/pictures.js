f3270f731e046ed2993010d916f187d4c9020bca
Khudorozhkova <E.D.Khudorozhkova@urfu.me>
2022-10-27 00:37:38 +0500

    pictures

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,6 @@
 import {isEscapeKey} from './util.js';
 
+const pictures = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = document.querySelector('.big-picture__img img');
 const likesCount = document.querySelector('.likes-count');
